e682e20a5db2f5cf0711446fff3afd1c4c4e2e21
George <rahulsapnaji@gmail.com>
2025-05-09 16:21:48 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './Assets/Scss/Custom.scss';
-import {} from 'react-router-dom';
-import {} from '@tanstack/react-query';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
+  const Queryclient = new QueryClient();
+  const Router = createBrowserRouter(
+    createRoutesFromElements(<Route path="/" element={<App />}></Route>),
+  );
+
   root.render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={Queryclient}>
+        <RouterProvider router={Router}></RouterProvider>
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }
